d195ae505cceac3e535da03ac41a289a08915778
raul0115 <raul0115@gmail.com>
2016-01-16 09:34:04 -0430

    integrando sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp= require('gulp');
 var markdown = require('gulp-markdown');
 var connect = require('gulp-connect');
+var sass = require('gulp-sass');
+var autoprefixer = require('autoprefixer');
+var postcss = require('gulp-postcss');
+var plumber = require('gulp-plumber');
 
 /**
  * Variable de entorno.
@@ -17,7 +21,23 @@ gulp.task('markdown', function() {
             return f.base;
         }));
 });
+/**
+ * Compila los archivos sass hijos directos de la carpeta `scss/`.
+ * Agrega los prefijos propietarios de los navegadores.
+ * Los archivos CSS generados se guardan en la carpeta `css/`.
+ */
+gulp.task('sass', function () {
+  var processors = [
+    autoprefixer({ browsers: ['last 2 versions'] })
+  ];
 
+  return gulp.src('./taller/scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(plumber())
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./taller/css'))
+    .pipe(connect.reload());
+});
 
 /**
  * Recarga el HTML en el navegador.
@@ -48,6 +68,15 @@ gulp.task('watch:markdown', ['markdown'], function () {
 });
 
 /**
+ * Ejecuta las tareas connect y sass, queda escuchando los cambios de todos
+ * los archivos Sass de la carpeta `scss/` y subcarpetas.
+ */
+gulp.task('watch:sass', ['connect', 'sass'], function () {
+  gulp.watch('./taller/scss/**/*.scss', ['sass']);
+});
+
+
+/**
  * Ejecuta las tareas connect y html, queda escuchando los cambios de todos
  * los archivos HTML de la carpeta raíz del proyecto.
  * Creado para quienes no usen Jade.
@@ -59,10 +88,10 @@ gulp.task('watch:html', ['connect', 'html'], function () {
 /**
  * Ejecuta las tareas html, markdown,connect
  */
-gulp.task('watch:all', ['watch:html', 'watch:markdown']);
+gulp.task('watch:all', ['watch:html', 'watch:markdown','watch:sass']);
 
 
 /**
- * Ejecuta la tarea watch:makdown y watch:html
+ * Ejecuta la tarea watch:makdown , watch:html watch:sass
  */
-gulp.task('default', ['watch:html','watch:markdown']);
+gulp.task('default', ['watch:html','watch:markdown','watch:sass']);
