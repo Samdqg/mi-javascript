abb5ab7b3768858dd8c4bb2a5ab72afeb5584efd
raul0115 <raul0115@gmail.com>
2016-01-16 08:45:04 -0430

    Entorno de trabajo mejorado

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 var gulp= require('gulp');
 var markdown = require('gulp-markdown');
+var connect = require('gulp-connect');
+
+/**
+ * Variable de entorno.
+ * En la terminal se puede definir de manera opcional el puerto para cualquiera
+ * de las tareas watch, un ejemplo de uso sería:
+ * PORT=8080 gulp watch:all
+ */
+ var PORT = process.env.PORT || 7070;
 
 gulp.task('markdown', function() {
     return gulp.src('**/*.md')
@@ -8,6 +17,53 @@ gulp.task('markdown', function() {
             return f.base;
         }));
 });
-gulp.task('default', function() {
-    gulp.watch('**/*.md', ['markdown']);
+
+
+/**
+ * Recarga el HTML en el navegador.
+ * Creado para quienes no usen Jade.
+ */
+gulp.task('html', function () {
+  gulp.src('./*.html')
+    .pipe(connect.reload());
 });
+
+
+/**
+ * Crea un servidor local livereload
+ * http://localhost:7070
+ */
+gulp.task('connect', function() {
+  connect.server({
+    // root: '.',
+    port: PORT,
+    livereload: true
+  });
+})
+
+/**
+ * Ejecuta la tarea markdown
+ */
+gulp.task('watch:markdown', ['markdown'], function () {
+  gulp.watch('**/*.md', ['markdown']);
+});
+
+/**
+ * Ejecuta las tareas connect y html, queda escuchando los cambios de todos
+ * los archivos HTML de la carpeta raíz del proyecto.
+ * Creado para quienes no usen Jade.
+ */
+gulp.task('watch:html', ['connect', 'html'], function () {
+  gulp.watch('./*.html', ['html']);
+});
+
+/**
+ * Ejecuta las tareas html, markdown,connect
+ */
+gulp.task('watch:all', ['watch:html', 'watch:markdown']);
+
+
+/**
+ * Ejecuta la tarea watch:makdown y watch:html
+ */
+gulp.task('default', ['watch:html','watch:markdown']);
